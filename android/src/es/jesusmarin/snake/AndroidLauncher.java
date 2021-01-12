c27813a6a6b6f78573335651b533064839af30e1
DavidRojoM <davidrojo1997@gmail.com>
2021-01-12 20:02:47 +0100

    Version inicial Snake

---
+++ android/src/es/jesusmarin/snake/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package es.jesusmarin.snake;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import es.jesusmarin.snake.GdxGameSnake;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxGameSnake(), config);
+	}
+}
---
+++ desktop/src/es/jesusmarin/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.jesusmarin.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.jesusmarin.snake.GdxGameSnake;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGameSnake(), config);
+	}
+}
